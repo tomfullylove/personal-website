002866303348770ff1d43bbb407ce3a16a2e8419
Tom Fullylove <t.fullylove@hotmail.co.uk>
2023-03-19 22:09:12 +0000

    Add icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-          <link rel="icon" href="#" />
+          <link rel="icon" href="/tf.ico" />
         </Head>
         <body>
           <Main />
