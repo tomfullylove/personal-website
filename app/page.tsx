159f4402d482695fad0d921a8329537fff4a57be
Tom Fullylove <t.fullylove@hotmail.co.uk>
2025-03-06 13:40:00 +0000

    Add base nextjs app with shadcn

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { AuroraText } from "@/components/magicui/aurora-text";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-9xl font-bold">
+      <AuroraText>full-stack</AuroraText> engineer
+      </h1>
+    </div>
+  );
+}
